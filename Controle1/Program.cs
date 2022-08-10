5059e8ffd848dd987d0ba3a061378c4a6724d9d3
Alex Romadin <chrono001@mail.ru>
2022-08-10 18:20:17 +0300

    Programm complete

--- Controle1/Program.cs
+++ Controle1/Program.cs
@@ -21,5 +21,5 @@ void PrintArray(string[] array)
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
 
-SecondArrayWithIF(array1, array2);
+MainMethod(array1, array2);
 PrintArray(array2);
