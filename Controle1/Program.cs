c899acf8f202eae7336c83de08c64b3ee6060813
Alex Romadin <chrono001@mail.ru>
2022-08-10 18:18:47 +0300

    PrintArrayMethod added

--- Controle1/Program.cs
+++ Controle1/Program.cs
@@ -10,3 +10,11 @@
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
